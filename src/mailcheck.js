9c4112d0df63550e1d188ad88ed7d387956b7105
Kleber Anderson <kleber7777@gmail.com>
2015-11-11 15:31:11 -0200

    add brazilian email domains

--- src/mailcheck.js
+++ src/mailcheck.js
@@ -13,23 +13,12 @@ var Mailcheck = {
   secondLevelThreshold: 2,
   topLevelThreshold: 2,
 
-  defaultDomains: ['msn.com', 'bellsouth.net',
-    'telus.net', 'comcast.net', 'optusnet.com.au',
-    'earthlink.net', 'qq.com', 'sky.com', 'icloud.com',
-    'mac.com', 'sympatico.ca', 'googlemail.com',
-    'att.net', 'xtra.co.nz', 'web.de',
-    'cox.net', 'gmail.com', 'ymail.com',
-    'aim.com', 'rogers.com', 'verizon.net',
-    'rocketmail.com', 'google.com', 'optonline.net',
-    'sbcglobal.net', 'aol.com', 'me.com', 'btinternet.com',
-    'charter.net', 'shaw.ca'],
+  defaultDomains: ["google.com", "hotmail.com", "gmail.com", "me.com", "terra.com.br", "yahoo.com", "yahoo.com.br", "bol.com.br",
+    "live.com",  "googlemail.com", "msn.com", "facebook.com", "outlook.com", "icloud.com"],
 
-  defaultSecondLevelDomains: ["yahoo", "hotmail", "mail", "live", "outlook", "gmx"],
+  defaultSecondLevelDomains: ["yahoo", "hotmail", "mail", "live", "outlook"],
 
-  defaultTopLevelDomains: ["com", "com.au", "com.tw", "ca", "co.nz", "co.uk", "de",
-    "fr", "it", "ru", "net", "org", "edu", "gov", "jp", "nl", "kr", "se", "eu",
-    "ie", "co.il", "us", "at", "be", "dk", "hk", "es", "gr", "ch", "no", "cz",
-    "in", "net", "net.au", "info", "biz", "mil", "co.jp", "sg", "hu"],
+  defaultTopLevelDomains: ["co.jp", "co.uk", "com", "net", "org", "info", "edu", "gov", "mil", "ca", "com.br"],  
 
   run: function(opts) {
     opts.domains = opts.domains || Mailcheck.defaultDomains;
